841bf5bdea1478fda970f8427474e9aab264453c
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-30 14:08:43 -0300

    script

---
+++ apps/script.js
@@ -0,0 +1,18 @@
+const animals = document.getElementById('animals');
+console.log(animals);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[1]);
+
+const liFast = document.querySelector('li');
+console.log(liFast);
+
+const ulFast = document.querySelector('ul');
+console.log(ulFast);
+
+const internalLink = document.querySelector('[href^="#"]');
+console.log(internalLink);
+
+const animalsImg = document.querySelectorAll('.animals img');
+console.log(animalsImg);
+
